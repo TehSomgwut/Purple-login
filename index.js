55c711cbc63c89aad807419da68becede8f28d61
TehSomgwut <phosrisongwut@gmail.com>
2025-07-11 16:47:00 +0700

    Adding express and nodemon and changing index.js to express instead.

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const http = require('http')
-const fs = require('fs')
-const server = http.createServer(function(request, response) {
-    let url = request.url
-    switch(url) {
-        case '/' : response.write('<h1>Empthy</h1>'); response.end(); break;
-        case '/signup/' : fs.readFile(__dirname + '/index.html', 'UTF-8', (err, data) =>{
-            if(err) {
-                response.writeHead(200, {'content-type' : 'text/html'})
-                response.end('<h1>Error<h1>', err.message)
-            }
-            else {
-                response.writeHead(200, {'content-type' : 'text/html'})
-                response.end(data)
-            }
-        }); break;
-        default:
-            response.writeHead(404, {'content-type' : 'text/html'})
-            response.end("No"); break;
-    }
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/signup', (request, response) => {
+    response.status(200)
+    response.type('text/html')
+    response.sendFile(path.join(__dirname, 'index.html'))
 })
 
-server.listen(3000)
-console.log("Server is running on port 3000")
+app.use('/', (request, response) => {
+    response.status(200)
+    response.type('text/html')
+    response.send("<h1>Empthy</h1>")
+})
+
+app.listen(3000)
+console.log("Starting server at port 3000")
